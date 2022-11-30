12eb5acf836dc7397f57b66e656e05fbd2d54867
julianaando <juandocezare@gmail.com>
2022-11-30 01:04:06 -0300

    Exercícios parte 1 feitos

--- Fundamentos/Higher-Order-Functions-do-JavaScript-ES6/01-Introducao-a-HOF/Exercício-oficial/Parte-01-script.js
+++ Fundamentos/Higher-Order-Functions-do-JavaScript-ES6/01-Introducao-a-HOF/Exercício-oficial/Parte-01-script.js
@@ -32,14 +32,35 @@ const sorteio = (aposta, funcao) => {
 // console.log(sorteio(2, checarNumero));
 
 /* Corretor automático de exame 
+
 Crie uma HOF que receberá três parâmetros:
 --- O primeiro será um array de respostas corretas (soluções);
 --- O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
 --- O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. Para isso, essa função usará os seguintes critérios:
------- Uma resposta correta adiciona 1 ponto à pontuação final;
+------ Uma resposta correta adiciona 1 ponto à pontuação final total+=1;
 ------ A ausência de uma resposta não altera a pontuação (quando for “N.A”);
------- Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
------- Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays: */
+------ Uma resposta incorreta reduz a pontuação final em 0.5 ponto. total-=0.5
+------ Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays: return total pontos */
 
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnswers = (rightAnswers, studentAnswers) => {
+  if (rightAnswers === studentAnswers) {
+    return 1;
+  } if (studentAnswers === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+
+const countPoints = (rightAnswers, studentAnswers, callback) => {
+  let totalPontos = 0;
+  for (let i = 0; i < rightAnswers.length; i += 1) {
+    const callbackReturn = callback(rightAnswers[i], studentAnswers[i]);
+    totalPontos += callbackReturn;
+  }
+  return `Resultado final: ${totalPontos} pontos`;
+};
+
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
