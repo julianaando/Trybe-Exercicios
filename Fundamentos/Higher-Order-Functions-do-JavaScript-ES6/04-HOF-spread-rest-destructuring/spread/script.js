581ca95df0686099f51e2aae97871b14f00b9234
Juliana <juandocezare@gmail.com>
2022-12-06 16:07:23 -0300

    Ex spread feito

---
+++ Fundamentos/Higher-Order-Functions-do-JavaScript-ES6/04-HOF-spread-rest-destructuring/spread/script.js
@@ -0,0 +1,13 @@
+/* Faça uma função chamada fruitSalad, passando como parâmetro specialFruit e additionalItens; faça a função retornar uma lista única, contendo todos os itens da nossa salada de frutas, usando o spread. */
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Banana', 'Mexerica', 'Abacaxi'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Uva', 'Morango', 'Pêra'];
+
+const fruitSalad = (fruit, additional) => {
+  return favoriteFruits = [...fruit, ...additional];
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
